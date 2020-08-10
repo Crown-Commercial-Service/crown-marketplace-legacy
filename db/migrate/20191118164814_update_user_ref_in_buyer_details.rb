bde38db8e70823c9fd61ea460b6daf299f45a4f6
Chris Gannon <cjgannon@gmail.com>
2020-08-10 09:15:41 +0100

    Restore static data task and update test database schema

--- db/migrate/20191118164814_update_user_ref_in_buyer_details.rb
+++ db/migrate/20191118164814_update_user_ref_in_buyer_details.rb
@@ -1,8 +1,6 @@
 class UpdateUserRefInBuyerDetails < ActiveRecord::Migration[5.2]
   def change
     # deleting all buyer details to make sure we get rid of inconsistencies
-    FacilitiesManagement::BuyerDetail.destroy_all
-
     change_table :facilities_management_buyer_details do |t|
       # rubocop:disable Rails/ReversibleMigration
       t.remove :user_id
