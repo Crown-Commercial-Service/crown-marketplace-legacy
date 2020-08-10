a1c43094fa100d28003742e55e7aa852044202ef
Chris Gannon <cjgannon@gmail.com>
2020-08-10 13:07:07 +0100

    Add static task back

--- bin/docker-entrypoint.sh
+++ bin/docker-entrypoint.sh
@@ -3,6 +3,10 @@
 # because the APP_RUN_SIDEKIQ parameter is defined in /Environment/ccs/cmp and not /Environment/ccs/cmpsidekiq,
 # we have to check for it being false, so we can run the rails server, otherwise run sidekiq
 if [ "$APP_RUN_SIDEKIQ" = 'FALSE' ]; then
+  if [ "$APP_RUN_STATIC_TASK" = 'TRUE' ]; then
+    bundle exec rails db:static
+  fi
+
   bundle exec rails server
 else
   bundle exec sidekiq -C ./config/sidekiq.yml -e production
