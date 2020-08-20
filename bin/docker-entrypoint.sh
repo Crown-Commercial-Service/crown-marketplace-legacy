1171cb34997a90d957953b22b9c22b7fe95e66c9
Chris Gannon <cjgannon@gmail.com>
2020-08-20 14:17:40 +0100

    Include asset sync

--- bin/docker-entrypoint.sh
+++ bin/docker-entrypoint.sh
@@ -2,6 +2,10 @@
 
 # because the APP_RUN_SIDEKIQ parameter is defined in /Environment/ccs/cmp and not /Environment/ccs/cmpsidekiq,
 # we have to check for it being false, so we can run the rails server, otherwise run sidekiq
+if [ "$APP_RUN_PRECOMPILE_ASSETS" = 'TRUE' ]; then
+  bundle exec rake assets:sync
+fi
+
 if [ "$APP_RUN_SIDEKIQ" = 'FALSE' ]; then
   if [ "$APP_RUN_STATIC_TASK" = 'TRUE' ]; then
     bundle exec rails db:static
