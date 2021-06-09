d15ffc7aa1536bfaf6a97f82a8fd960c4a757295
tim-s-ccs <timothy.south@crowncommercial.gov.uk>
2021-06-09 13:00:14 +0100

    Allow for MCF3 data to be blank before live date

--- app/services/management_consultancy/files_checker.rb
+++ app/services/management_consultancy/files_checker.rb
@@ -20,29 +20,41 @@ class ManagementConsultancy::FilesChecker
 
   def check_supplier_details_spreadsheet(suppliers_workbook)
     check_sheets(suppliers_workbook, supplier_details_sheets, 'supplier_details') do |sheets_with_errors, empty_sheets, index|
+      framework = "MCF#{index_to_framework[index]}"
+
       if suppliers_workbook.sheet(index).row(1) != ['Supplier name', 'Email address', 'Phone number', 'Website URL', 'Postal address', 'Is an SME?', 'DUNS Number']
-        sheets_with_errors << "MCF#{index_to_framework[index]}"
+        sheets_with_errors << framework
       elsif suppliers_workbook.sheet(index).last_row == 1
-        empty_sheets << "MCF#{index_to_framework[index]}"
+        next if mcf3_can_be_blank?(framework)
+
+        empty_sheets << framework
       end
     end
   end
 
   def check_supplier_rate_cards_spreadsheet(rate_cards_workbook)
     check_sheets(rate_cards_workbook, all_sheets, 'supplier_rate_cards') do |sheets_with_errors, empty_sheets, index|
+      service_code = index_to_service_code(index)
+
       if rate_cards_workbook.sheet(index).last_column != 10
-        sheets_with_errors << index_to_service_code(index)
+        sheets_with_errors << service_code
       elsif rate_cards_workbook.sheet(index).last_row == 1
-        empty_sheets << index_to_service_code(index)
+        next if mcf3_can_be_blank?(service_code)
+
+        empty_sheets << service_code
       end
     end
   end
 
   def check_supplier_service_offerings_spreadsheet(service_offerings_workbook)
     check_sheets(service_offerings_workbook, all_sheets, 'supplier_service_offerings') do |sheets_with_errors, empty_sheets, index|
+      service_code = index_to_service_code(index)
+
       if service_offerings_workbook.sheet(index).last_row != sheet_index_to_service_code[index][:rows]
         sheets_with_errors << index_to_service_code(index)
       elsif service_offerings_workbook.sheet(index).last_column == 1
+        next if mcf3_can_be_blank?(service_code)
+
         empty_sheets << index_to_service_code(index)
       end
     end
@@ -50,6 +62,10 @@ class ManagementConsultancy::FilesChecker
 
   def index_to_service_code(index)
     "MCF#{sheet_index_to_service_code[index][:mcf]} Lot #{sheet_index_to_service_code[index][:code]}"
+  end
+
+  def mcf3_can_be_blank?(code)
+    !Marketplace.mcf3_live? && code.start_with?('MCF3')
   end
 
   def supplier_details_sheets
