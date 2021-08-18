1b3fd592534e5267ff9e5d5fb88d8c441b507342
tim-s-ccs <timothy.south@crowncommercial.gov.uk>
2021-08-18 13:14:33 +0100

    Update the date validation
    Add a concern for the dates
    
    Add all the date validations into concern

--- app/models/supply_teachers/journey/fixed_term_results.rb
+++ app/models/supply_teachers/journey/fixed_term_results.rb
@@ -1,7 +1,10 @@
 module SupplyTeachers
   class Journey::FixedTermResults
+    DATE_ATTIBUTES = %i[contract_start_date contract_end_date].freeze
+
     include Steppable
     include Dateable
+    include DateValidator
     include Journey::Results
     include ActiveSupport::NumberHelper
 
@@ -11,8 +14,6 @@ module SupplyTeachers
     attribute :contract_end_date_day
     attribute :contract_end_date_month
     attribute :contract_end_date_year
-
-    PARSED_DATE_FORMAT = '%Y-%m-%d'.freeze
 
     def rates
       Rate.direct_provision.fixed_term
@@ -34,26 +35,6 @@ module SupplyTeachers
         postcode: postcode,
         radius: number_to_human(radius, units: :miles)
       }
-    end
-
-    private
-
-    def contract_end_date
-      Date.strptime(
-        "#{contract_end_date_year}-#{contract_end_date_month}-#{contract_end_date_day}",
-        PARSED_DATE_FORMAT
-      )
-    rescue ArgumentError
-      nil
-    end
-
-    def contract_start_date
-      Date.strptime(
-        "#{contract_start_date_year}-#{contract_start_date_month}-#{contract_start_date_day}",
-        PARSED_DATE_FORMAT
-      )
-    rescue ArgumentError
-      nil
     end
   end
 end
