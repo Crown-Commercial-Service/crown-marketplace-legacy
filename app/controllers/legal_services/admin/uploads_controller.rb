3c6c9733bd263344d1ffd7a5745c50ab8be905ad
tim-s-ccs <timothy.south@crowncommercial.gov.uk>
2021-08-18 13:13:23 +0100

    Remove the ls JavaScript
    Update the blobs controller
    
    Update the routes and upload controllers
    
    Remove unused controller and move code into concerns
    
    Add some more concerns

--- app/controllers/legal_services/admin/uploads_controller.rb
+++ app/controllers/legal_services/admin/uploads_controller.rb
@@ -1,13 +1,10 @@
 module LegalServices
   module Admin
     class UploadsController < LegalServices::FrameworkController
+      include ::Admin::SharedPagesConcern
       include ::Admin::UploadsController
 
       private
-
-      def service
-        LegalServices
-      end
 
       def upload_params
         params.require(:legal_services_admin_upload).permit(:supplier_details_file, :supplier_rate_cards_file, :supplier_lot_1_service_offerings_file, :supplier_lot_2_service_offerings_file, :supplier_lot_3_service_offerings_file, :supplier_lot_4_service_offerings_file) if params[:legal_services_admin_upload].present?
