23a0ef596d66b3d645a8804c925382de3a0c77c2
tim-s-ccs <timothy.south@crowncommercial.gov.uk>
2022-11-03 18:03:51 +0000

    Due to an issue with the legacy release 2.5.3, the database migration did not work due to models being removed. This has meant the admin sections for MC, LS and ST are not working.
    I have had to comment out the original migration as that will no longer work both due to the model being removed and an update to rails. I have then added a new migration which will fix the issue with the LS upload for real this time.
    
    I have tested it locally and it works, we will not be able to test this in the lower environments.

--- db/migrate/20220929130611_fix_issue_with_ls_uploads.rb
+++ db/migrate/20220929130611_fix_issue_with_ls_uploads.rb
@@ -1,24 +1,25 @@
 class FixIssueWithLsUploads < ActiveRecord::Migration[6.0]
+  # We have to turn off this migration due to an issue in Legacy Release 2.5.3
   def up
-    ActiveStorage::Attachment.where(record_type: 'LegalServices::Admin::Upload').find_in_batches do |attachment_group|
-      sleep(5)
+    # ActiveStorage::Attachment.where(record_type: 'LegalServices::Admin::Upload').find_in_batches do |attachment_group|
+    #   sleep(5)
 
-      attachment_group.each do |attachment|
-        new_record_type = ''
+    #   attachment_group.each do |attachment|
+    #     new_record_type = ''
 
-        if LegalServices::RM3788::Admin::Upload.find_by(id: attachment.record_id)
-          new_record_type = 'LegalServices::RM3788::Admin::Upload'
-        elsif LegalServices::RM6240::Admin::Upload.find_by(id: attachment.record_id)
-          new_record_type = 'LegalServices::RM6240::Admin::Upload'
-        else
-          Rails.logger.warn("Could not find LegalServices::Admin::Upload for #{attachment.record_id}")
+    #     if LegalServices::RM3788::Admin::Upload.find_by(id: attachment.record_id)
+    #       new_record_type = 'LegalServices::RM3788::Admin::Upload'
+    #     elsif LegalServices::RM6240::Admin::Upload.find_by(id: attachment.record_id)
+    #       new_record_type = 'LegalServices::RM6240::Admin::Upload'
+    #     else
+    #       Rails.logger.warn("Could not find LegalServices::Admin::Upload for #{attachment.record_id}")
 
-          next
-        end
+    #       next
+    #     end
 
-        attachment.update(record_type: new_record_type)
-      end
-    end
+    #     attachment.update(record_type: new_record_type)
+    #   end
+    # end
   end
 
   def down; end
