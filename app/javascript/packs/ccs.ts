b06ef6dcaa23ba0b40ab270aeda36b13bea212b2
tim-s-ccs <timothy.south@crowncommercial.gov.uk>
2023-09-11 09:05:15 +0100

    Remove google analytics (not Google Tag Manager)
    Add GTM to the CSP to prevent XSS

--- app/javascript/packs/ccs.ts
+++ app/javascript/packs/ccs.ts
@@ -1,7 +1,6 @@
 import initAdminUpload from '../src/shared/adminUpload'
 import initBasket from '../src/shared/basket'
 import initCookieBanner from '../src/shared/cookieBanner'
-import initGoogleAnalytics from '../src/shared/googleAnalytics'
 import initPasswordStrength from '../src/shared/passwordStrength'
 import initSupplyTeachersAdminUpload from '../src/supplyTeachers/adminUpload'
 import initSupplyTeachersSupplierMarkupCalculator from '../src/supplyTeachers/supplierMarkupCalculator'
@@ -10,7 +9,6 @@ $(document).on('turbolinks:load', () => {
   initAdminUpload()
   initBasket()
   initCookieBanner()
-  initGoogleAnalytics()
   initPasswordStrength()
 
   initSupplyTeachersAdminUpload()
