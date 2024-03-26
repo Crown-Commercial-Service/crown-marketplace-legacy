eb384e8fa665ad9198db0256610d5b93ca6557ef
tim-s-ccs <timothy.south@crowncommercial.gov.uk>
2024-03-26 09:52:20 +0000

    Update the ccs frontend helpers package and make updates to the app to accommodate

--- spec/helpers/management_consultancy/rm6187/admin/uploads_helper_spec.rb
+++ spec/helpers/management_consultancy/rm6187/admin/uploads_helper_spec.rb
@@ -83,64 +83,64 @@ RSpec.describe ManagementConsultancy::RM6187::Admin::UploadsHelper do
     context 'when the status is not_started' do
       let(:status) { 'not_started' }
 
-      it 'returns in progress and grey' do
-        expect(status_tag).to eq ['in progress', :grey]
+      it 'returns In progress and grey' do
+        expect(status_tag).to eq ['In progress', :grey]
       end
     end
 
     context 'when the status is in_progress' do
       let(:status) { 'in_progress' }
 
-      it 'returns in progress and grey' do
-        expect(status_tag).to eq ['in progress', :grey]
+      it 'returns In progress and grey' do
+        expect(status_tag).to eq ['In progress', :grey]
       end
     end
 
     context 'when the status is checking_files' do
       let(:status) { 'checking_files' }
 
-      it 'returns in progress and grey' do
-        expect(status_tag).to eq ['in progress', :grey,]
+      it 'returns In progress and grey' do
+        expect(status_tag).to eq ['In progress', :grey,]
       end
     end
 
     context 'when the status is processing_files' do
       let(:status) { 'processing_files' }
 
-      it 'returns in progress and grey' do
-        expect(status_tag).to eq ['in progress', :grey]
+      it 'returns In progress and grey' do
+        expect(status_tag).to eq ['In progress', :grey]
       end
     end
 
     context 'when the status is checking_processed_data' do
       let(:status) { 'checking_processed_data' }
 
-      it 'returns in progress and grey' do
-        expect(status_tag).to eq ['in progress', :grey]
+      it 'returns In progress and grey' do
+        expect(status_tag).to eq ['In progress', :grey]
       end
     end
 
     context 'when the status is publishing_data' do
       let(:status) { 'publishing_data' }
 
-      it 'returns in progress and grey' do
-        expect(status_tag).to eq ['in progress', :grey]
+      it 'returns In progress and grey' do
+        expect(status_tag).to eq ['In progress', :grey]
       end
     end
 
     context 'when the status is published' do
       let(:status) { 'published' }
 
-      it 'returns published on live' do
-        expect(status_tag).to eq ['published on live']
+      it 'returns Published on live' do
+        expect(status_tag).to eq ['Published on live']
       end
     end
 
     context 'when the status is failed' do
       let(:status) { 'failed' }
 
-      it 'returns failed and red' do
-        expect(status_tag).to eq ['failed', :red]
+      it 'returns Failed and red' do
+        expect(status_tag).to eq ['Failed', :red]
       end
     end
   end
