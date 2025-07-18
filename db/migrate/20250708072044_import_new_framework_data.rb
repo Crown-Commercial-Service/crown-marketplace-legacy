a18a32715e2d82a53c2bd5cd4593d4d176f63151
tim-s-ccs <timothy.south@crowncommercial.gov.uk>
2025-07-18 12:41:18 +0100

    Disable the invocation of update_frameworks except for the last instance that has not been run in production

--- db/migrate/20250708072044_import_new_framework_data.rb
+++ db/migrate/20250708072044_import_new_framework_data.rb
@@ -1,6 +1,7 @@
 class ImportNewFrameworkData < ActiveRecord::Migration[8.0]
   def up
-    Rake::Task['db:update_frameworks'].invoke
+    # Disabled as we only want the last instance of this to be run on any given environment
+    # Rake::Task['db:update_frameworks'].invoke
   end
 
   def down; end
