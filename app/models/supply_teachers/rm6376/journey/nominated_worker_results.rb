c2b0d62695a4018ec9a54ca513a5ac353d1e230f
chloeprecaj <chloe.precaj1@crowncommercial.gov.uk>
2026-03-31 15:53:38 +0100

    unit test for workers

---
+++ app/models/supply_teachers/rm6376/journey/nominated_worker_results.rb
@@ -0,0 +1,7 @@
+module SupplyTeachers
+  module RM6376
+    class Journey::NominatedWorkerResults < SupplyTeachers::Journey::NominatedWorkerResults
+      include Journey::Results
+    end
+  end
+end
