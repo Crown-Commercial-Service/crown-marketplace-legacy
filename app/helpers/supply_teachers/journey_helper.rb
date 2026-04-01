144a759ded7ffc00eb8017228ee0513cb1223ed9
chloeprecaj <chloe.precaj1@crowncommercial.gov.uk>
2026-04-01 16:33:24 +0100

    postcode page

--- app/helpers/supply_teachers/journey_helper.rb
+++ app/helpers/supply_teachers/journey_helper.rb
@@ -3,12 +3,19 @@ module SupplyTeachers::JourneyHelper
     case params[:framework]
     when 'RM6238'
       Position.where(lot_id: 'RM6238.1', number: [1, 3, 5, 7]).order(:number)
+    when 'RM6376'
+      Position.where(lot_id: 'RM6376.1', number: [1, 2, 3,4, 5, 6, 7, 8]).order(:number)
     end
   end
 
   def offsets
     case params[:framework]
     when 'RM6238'
+      [
+        [0, 'daily'],
+        [1, 'six_weeks_plus'],
+      ]
+    when 'RM6376'
       [
         [0, 'daily'],
         [1, 'six_weeks_plus'],
