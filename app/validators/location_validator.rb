9ee56c6f0911df049231295203240119f4eff41f
tim-s-ccs <timothy.south@crowncommercial.gov.uk>
2026-04-09 14:33:40 +0100

    Clean up some files and fix issue with postcode validations

--- app/validators/location_validator.rb
+++ app/validators/location_validator.rb
@@ -1,7 +1,7 @@
 class LocationValidator < ActiveModel::EachValidator
-  def validate_each(record, attribute, value)
+  def validate_each(record, _attribute, value)
     return if value.found?
 
-    record.errors.add attribute, :invalid_location
+    record.errors.add :postcode, :invalid_location
   end
 end
