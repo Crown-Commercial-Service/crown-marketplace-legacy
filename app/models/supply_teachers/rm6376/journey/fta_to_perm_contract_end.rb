be9132748a4964cc2a9ab67037a1a6d9e8e1cf93
chloeprecaj <chloe.precaj1@crowncommercial.gov.uk>
2026-04-14 16:24:27 +0100

    Find out how much you’ll be charged if you make an agency worker permanent page questions

---
+++ app/models/supply_teachers/rm6376/journey/fta_to_perm_contract_end.rb
@@ -0,0 +1,6 @@
+module SupplyTeachers
+  module RM6376
+    class Journey::FTAToPermContractEnd < SupplyTeachers::Journey::FTAToPermContractEnd
+    end
+  end
+end
---
+++ app/models/supply_teachers/rm6376/journey/fta_to_perm_fixed_term_fee.rb
@@ -0,0 +1,6 @@
+module SupplyTeachers
+  module RM6376
+    class Journey::FTAToPermFixedTermFee < SupplyTeachers::Journey::FTAToPermFixedTermFee
+    end
+  end
+end
