7e4d2332e5e1f7bf8b7c853696f1f81e9a6feedf
chloeprecaj <chloe.precaj1@crowncommercial.gov.uk>
2026-04-15 11:30:51 +0100

    Find out how much you’ll be charged if you make an agency worker permanent page questions

---
+++ spec/models/supply_teachers/rm6376/journey/temp_to_perm_fee_spec.rb
@@ -0,0 +1,9 @@
+require 'rails_helper'
+
+RSpec.describe SupplyTeachers::RM6376::Journey::TempToPermFee do
+  subject(:step) { described_class.new }
+
+  it 'is the final step' do
+    expect(step.final?).to be true
+  end
+end
