12096d190733a79c717e15cf7385537a27c690c3
chloeprecaj <chloe.precaj1@crowncommercial.gov.uk>
2026-04-21 12:51:54 +0100

    agency worker feature

--- app/models/supply_teachers/rm6238/journey/fixed_term_results.rb
+++ app/models/supply_teachers/rm6238/journey/fixed_term_results.rb
@@ -2,10 +2,6 @@ module SupplyTeachers
   module RM6238
     class Journey::FixedTermResults < SupplyTeachers::Journey::FixedTermResults
       include Journey::Results
-
-      def framework
-        'RM6238'
-      end
     end
   end
 end
--- app/models/supply_teachers/rm6376/journey/agency_payroll_results.rb
+++ app/models/supply_teachers/rm6376/journey/agency_payroll_results.rb
@@ -9,6 +9,16 @@ module SupplyTeachers
       def determine_position_id
         @determine_position_id ||= "RM6376.1.#{position_number.to_i}"
       end
+
+      def inputs
+        {
+          looking_for: translate_input('supply_teachers.looking_for.worker'),
+          worker_type: translate_input('supply_teachers.worker_type.agency_supplied'),
+          payroll_provider: translate_input('supply_teachers.payroll_provider.agency'),
+          postcode: postcode,
+          radius: number_to_human(radius, units: :miles),
+        }
+      end
     end
   end
 end
