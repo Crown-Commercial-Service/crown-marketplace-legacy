e4f21c4a74a72edf864a18dc6bcbb15ec32afb14
chloeprecaj <chloe.precaj1@crowncommercial.gov.uk>
2026-04-22 15:16:50 +0100

    agency worker add back translation for what to do nexr

--- app/models/supply_teachers/rm6238/journey/agency_payroll_results.rb
+++ app/models/supply_teachers/rm6238/journey/agency_payroll_results.rb
@@ -1,7 +1,6 @@
 module SupplyTeachers
   module RM6238
     class Journey::AgencyPayrollResults < SupplyTeachers::Journey::AgencyPayrollResults
-
       include Journey::Results
 
       def determine_position_id
