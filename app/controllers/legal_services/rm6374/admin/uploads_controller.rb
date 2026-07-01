34fd7cfc4a017023177b99388a319afdf55f334b
chloeprecaj <chloe.precaj1@crowncommercial.gov.uk>
2026-07-01 12:09:23 +0100

    fix error when uploading

--- app/controllers/legal_services/rm6374/admin/uploads_controller.rb
+++ app/controllers/legal_services/rm6374/admin/uploads_controller.rb
@@ -11,10 +11,10 @@ module LegalServices
         end
 
         def upload_params
-          params.require(:legal_services_rm6374_admin_upload).permit(
-            :supplier_details_file,
-            :supplier_geographical_data_file,
-            :supplier_rate_cards_file
+          params.expect(
+            legal_services_rm6374_admin_upload: %i[supplier_details_file
+                                                   supplier_geographical_data_file
+                                                   supplier_rate_cards_file]
           )
         end
       end
