22be66ec8df94571e74cbbcc287c2e37fbe5fd52
chloeprecaj <chloe.precaj1@crowncommercial.gov.uk>
2026-07-02 12:59:51 +0100

    rubocop

--- app/workers/legal_services/rm6374/file_upload_worker.rb
+++ app/workers/legal_services/rm6374/file_upload_worker.rb
@@ -1,9 +1,7 @@
 module LegalServices
   module RM6374
-    module Admin
-      class FileUploadWorker < ::Admin::FileUploadWorker
-        sidekiq_options queue: 'ls', retry: false
-      end
+    class FileUploadWorker < ::Admin::FileUploadWorker
+      sidekiq_options queue: 'ls', retry: false
     end
   end
 end
