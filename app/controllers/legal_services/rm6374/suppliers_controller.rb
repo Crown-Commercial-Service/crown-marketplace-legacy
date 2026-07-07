00449ad5740852ae511429bcca19f65f40a1d8e3
chloeprecaj <chloe.precaj1@crowncommercial.gov.uk>
2026-07-07 14:20:11 +0100

    jurisdiction

--- app/controllers/legal_services/rm6374/suppliers_controller.rb
+++ app/controllers/legal_services/rm6374/suppliers_controller.rb
@@ -1,6 +1,24 @@
 module LegalServices
   module RM6374
-    class SuppliersController < LegalServices::RM6374::FrameworkController
+    class SuppliersController < LegalServices::SuppliersController
+      private
+
+      def fetch_rates
+        @supplier_framework.grouped_rates_for_lot(@lot.id)
+      end
+
+      def fetch_supplier_frameworks
+        
+        service_codes = params.expect(service_numbers: []).map do |service_number| 
+          "#{@lot.id}.#{service_number}" 
+        end
+        
+        @supplier_frameworks = Supplier::Framework.with_services(service_codes).shuffle
+      end
+
+      def fetch_lot
+       @lot = Lot.find("RM6374.#{params.expect(:lot_number)}")   
+      end
     end
   end
 end
---
+++ app/models/legal_services/rm6374/journey/suppliers.rb
@@ -0,0 +1,7 @@
+module LegalServices
+  module RM6374
+    class Journey::Suppliers
+      include Steppable
+    end
+  end
+end
