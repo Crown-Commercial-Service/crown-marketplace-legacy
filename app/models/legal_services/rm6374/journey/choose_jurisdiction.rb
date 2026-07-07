2a9bab7aa72cc4cb45781f76524c60c190db7da4
chloeprecaj <chloe.precaj1@crowncommercial.gov.uk>
2026-07-07 11:20:35 +0100

    jurisdiction

--- app/models/legal_services/rm6374/journey/choose_jurisdiction.rb
+++ app/models/legal_services/rm6374/journey/choose_jurisdiction.rb
@@ -1,11 +1,10 @@
 module LegalServices
   module RM6374
     class Journey::ChooseJurisdiction < LegalServices::Journey::ChooseJurisdiction
+      attribute :lot_number, :string
 
-      attribute :lot_number, :string 
-      
       def lot
-        Lot.find("RM6374.#{lot_number}") 
+        Lot.find("RM6374.#{lot_number}")
       end
 
       def next_step_class
