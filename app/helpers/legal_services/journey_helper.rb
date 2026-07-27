f8a8bee39cd5d6e5698a04c3c80aa2feb377ba40
Chee-Ng <chee.ng@crowncommercial.gov.uk>
2026-07-27 16:27:18 +0100

    added helper method

--- app/helpers/legal_services/journey_helper.rb
+++ app/helpers/legal_services/journey_helper.rb
@@ -3,6 +3,11 @@ module LegalServices::JourneyHelper
     "Lot #{lot.number[0]} - #{lot.name}"
   end
 
+  def lot_full_name_via_number(lot_number)
+    lot = Lot.find("RM6374.#{lot_number}")
+    "Lot #{lot.number} - #{lot.name}"
+  end
+
   def lot_legal_services(lot_number)
     "Lot #{lot_number} legal services"
   end
