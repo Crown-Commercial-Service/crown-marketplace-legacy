747893cbe8173ba535629e8af8b3ca72d850378c
Chee-Ng <chee.ng@crowncommercial.gov.uk>
2026-08-06 11:10:01 +0100

    added upload function for RM6374 (missing translation)

--- app/models/legal_services/rm6374/admin/supplier_contact_detail.rb
+++ app/models/legal_services/rm6374/admin/supplier_contact_detail.rb
@@ -4,7 +4,7 @@ module LegalServices
       class SupplierContactDetail < ::Supplier::Framework::ContactDetail
         self.table_name = 'supplier_framework_contact_details'
 
-        ATTRIBUTES_TO_VALIDATE = %i[email telephone_number website address description lot_1_prospectus_link lot_1a_prospectus_link lot_1b_prospectus_link lot_1c_prospectus_link lot_2_prospectus_link lot_3_prospectus_link lot_4_prospectus_link lot_5_prospectus_link lot_6_prospectus_link].freeze
+        ATTRIBUTES_TO_VALIDATE = %i[email telephone_number website address description lot_1a_prospectus_link lot_1b_prospectus_link lot_1c_prospectus_link lot_2_prospectus_link lot_3_prospectus_link lot_4_prospectus_link lot_5_prospectus_link lot_6_prospectus_link].freeze
       end
     end
   end
