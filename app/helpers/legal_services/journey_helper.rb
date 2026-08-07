6e9ee1a5627c3e59c3cc6ff4359ced634e454209
chloeprecaj <chloe.precaj1@crowncommercial.gov.uk>
2026-08-07 16:01:55 +0100

    content changes

--- app/helpers/legal_services/journey_helper.rb
+++ app/helpers/legal_services/journey_helper.rb
@@ -2,9 +2,13 @@ module LegalServices::JourneyHelper
   include LegalServices::RatesHelper
 
   def lot_full_name(lot)
-    "Lot #{lot.number[0]} - #{lot.name}"
+    if params[:framework].to_s.casecmp('rm6374').zero?
+      "Lot #{lot.number} - #{lot.name}"
+    else
+      "Lot #{lot.number[0]} - #{lot.name}"
+    end
   end
-
+  
   def lot_full_name_via_number(lot_number)
     lot = Lot.find("RM6374.#{lot_number}")
     "Lot #{lot.number} - #{lot.name}"
