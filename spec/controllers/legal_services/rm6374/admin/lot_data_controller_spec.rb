35987c7711d1a38a920984ba2ec2547bcbea153c
chloeprecaj <chloe.precaj1@crowncommercial.gov.uk>
2026-08-18 12:02:04 +0100

    lot 5 name change

--- spec/controllers/legal_services/rm6374/admin/lot_data_controller_spec.rb
+++ spec/controllers/legal_services/rm6374/admin/lot_data_controller_spec.rb
@@ -82,7 +82,7 @@ RSpec.describe LegalServices::RM6374::Admin::LotDataController do
               sections: %i[services jurisdictions rates]
             },
             {
-              lot: { number: '5', number_as_slug: '5', name: 'Transport and Planning' },
+              lot: { number: '5', number_as_slug: '5', name: 'Transport and Rail' },
               enabled: nil,
               sections: %i[services jurisdictions rates]
             },
