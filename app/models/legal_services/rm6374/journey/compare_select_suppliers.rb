73b1316c1f0f4c7edcfe6d80ce415fa069bbe6d8
Chee-Ng <chee.ng@crowncommercial.gov.uk>
2026-09-03 14:57:58 +0100

    added checkboxes for supplier comparison

--- app/models/legal_services/rm6374/journey/compare_select_suppliers.rb
+++ app/models/legal_services/rm6374/journey/compare_select_suppliers.rb
@@ -17,20 +17,53 @@ module LegalServices
           Lot.find("RM6374.#{lot_number}")
         end
 
-        def available_suppliers
-          selected_services = get_service_numbers(lot_number)
+        def supplier_frameworks_with_rates
+          @supplier_frameworks_with_rates ||= supplier_frameworks.map do |supplier_framework|
+            rates = supplier_framework.grouped_rates_for_lot(
+              lot.id,
+            ).sort_by { |position_id, _| position_id }.to_h
+            [supplier_framework, rates]
+          end
+        end
 
-          return ::Supplier::Framework.with_lots(lot.id).with_services(selected_services).sort_by(&:supplier_name) if lot_number == '6'
+        def positions
+          all_positions = lot.positions.order(:number).pluck(:id, :name)
 
-          selected_jurisdiction_id = get_jurisdiction(jurisdiction)
+          return all_positions if professions.blank? || professions.include?('all')
 
-          ::Supplier::Framework.with_lots(lot.id)
-                               .with_services_and_jurisdiction(selected_services, [selected_jurisdiction_id])
-                               .sort_by(&:supplier_name)
+          all_positions.select do |position_id, profession_name|
+            professions.include?(profession_name) || professions.include?(position_id)
+          end
         end
 
         def next_step_class
           Journey::SuppliersComparison
+        end
+
+        private
+
+        def supplier_frameworks
+          selected_services = get_service_numbers(lot_number)
+
+          @supplier_frameworks ||= if lot_number == '6'
+                                     fetch_lot_6_supplier_frameworks(selected_services)
+                                   else
+                                     fetch_standard_supplier_frameworks(selected_services)
+                                   end
+        end
+
+        def fetch_lot_6_supplier_frameworks(selected_services)
+          scope = ::Supplier::Framework.with_lots(lot.id).with_services(selected_services)
+          scope = scope.where(id: supplier_framework_ids) if supplier_framework_ids.present?
+          scope.sort_by(&:supplier_name)
+        end
+
+        def fetch_standard_supplier_frameworks(selected_services)
+          selected_jurisdiction_id = get_jurisdiction(jurisdiction)
+          scope = ::Supplier::Framework.with_lots(lot.id)
+                                       .with_services_and_jurisdiction(selected_services, [selected_jurisdiction_id])
+          scope = scope.where(id: supplier_framework_ids) if supplier_framework_ids.present?
+          scope.sort_by(&:supplier_name)
         end
       end
     end
